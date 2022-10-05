ccd522c83a17b7eb5e2633aa832e0fd70810aaef
Haitham-Ibrahim <114887134+Haitham-Ibrahim@users.noreply.github.com>
2022-10-05 14:14:43 +0200

    training project

---
+++ partials-front/footer.php
@@ -0,0 +1,12 @@
+
+
+    <!-- footer Section Starts Here -->
+    <section class="footer">
+        <div class="container text-center">
+            <p style="font-size: 26px;" > <?php echo date('Y')?>جميع الحقوق محفوظة </p>
+        </div>
+    </section>
+    <!-- footer Section Ends Here -->
+
+</body>
+</html>
